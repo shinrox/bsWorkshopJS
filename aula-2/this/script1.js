1f53a1cc29855426426490ceb340d3316a4aabc3
Jonatas Gusmão <js.gusmao@hotmail.com>
2016-07-27 11:02:43 -0300

    Add exemplos

--- aula-2/this/script1.js
+++ aula-2/this/script1.js
@@ -28,4 +28,32 @@ var you = {
   greet: greet
 };
 
+// funciona da mesma forma que o exemplo anterior, a função chamada é a que está no contexto do objeto `you`
 you.greet();
+
+
+function multiply(n) {
+  return bsWorkshop.log(this * n);
+}
+
+
+// É possível passar qualquer tipo de variável para ser o this (contexto) de uma função,
+// além do call e apply que executam a função imediatamente
+// temos o bind que retorna uma nova função com o this configurado com o primeiro parâmetro passado
+// no exemplo abaixo, o this da nova função (multiply) será 2
+var multiplyByTwo = multiply.bind(2);
+
+// e quando executar a nova função criada, ela terá o this definido anteriormente para multiplicar 
+// pelo argumento sendo passado agora
+multiplyByTwo(3);
+
+
+// retorna NaN pq está tentando executar `window * 3` já que em nenhum momento foi definido outro this para a função multiply diretamente
+multiply(10);
+
+// retorna 20 ao ser executado
+multiply.call(2, 10);
+
+// retorna 40 ao ser executado
+multiply.apply(2, [20]);
+
